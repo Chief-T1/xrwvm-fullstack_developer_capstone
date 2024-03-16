547809806d189ed2e176a1712ae88e17af229e32
Chief-T1 <75487499+Chief-T1@users.noreply.github.com>
2024-03-16 19:49:46 -0400

    additional fix for add review view

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -131,6 +131,7 @@ def get_dealer_details(request, dealer_id):
 
 def add_review(request):
     if request.user.is_anonymous is False:
+        data = json.loads(request.body)
         try:
             response = post_review(data)
             return JsonResponse({"status": 200, "message": reponse.text})
